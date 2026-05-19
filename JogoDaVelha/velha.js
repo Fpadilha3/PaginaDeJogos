9b47cd3e95d7ead18ebbe7842c51798df844e0b5
Aluno São Lucas <aluno.saolucas@ensino.ulbra.net>
2026-05-19 17:24:49 -0300

    add projeto

--- JogoDaVelha/velha.js
+++ JogoDaVelha/velha.js
@@ -1,24 +1,47 @@
-const cells = document.querySelectorAll(".cell");
+const cells =
+document.querySelectorAll(".cell");
 
-const statusText = document.getElementById("status");
+const statusText =
+document.getElementById("status");
 
-const turnText = document.getElementById("turn");
+const turnText =
+document.getElementById("turn");
 
-const startBtn = document.getElementById("startBtn");
+const startBtn =
+document.getElementById("startBtn");
 
-const restartBtn = document.getElementById("restartBtn");
+const restartBtn =
+document.getElementById("restartBtn");
 
-const scoreXText = document.getElementById("scoreX");
+const resetScoreBtn =
+document.getElementById("resetScoreBtn");
 
-const scoreOText = document.getElementById("scoreO");
+const scoreXText =
+document.getElementById("scoreX");
 
-const playerXInput = document.getElementById("playerX");
+const scoreOText =
+document.getElementById("scoreO");
 
-const playerOInput = document.getElementById("playerO");
+const playerXInput =
+document.getElementById("playerX");
 
-const nameX = document.getElementById("nameX");
+const playerOInput =
+document.getElementById("playerO");
 
-const nameO = document.getElementById("nameO");
+const nameX =
+document.getElementById("nameX");
+
+const nameO =
+document.getElementById("nameO");
+
+const winnerPopup =
+document.getElementById("winnerPopup");
+
+const winnerTitle =
+document.getElementById("winnerTitle");
+
+const closePopup =
+document.getElementById("closePopup");
 
 let board = [
   "", "", "",
@@ -49,40 +72,75 @@ const winningCombinations = [
 
 ];
 
-startBtn.addEventListener("click", startGame);
+startBtn.addEventListener(
+  "click",
+  startGame
+);
 
-restartBtn.addEventListener("click", restartGame);
+restartBtn.addEventListener(
+  "click",
+  restartGame
+);
+
+resetScoreBtn.addEventListener(
+  "click",
+  resetScore
+);
+
+closePopup.addEventListener(
+  "click",
+  closeWinnerPopup
+);
 
 cells.forEach(cell => {
-  cell.addEventListener("click", handleCellClick);
+
+  cell.addEventListener(
+    "click",
+    handleCellClick
+  );
+
 });
 
 function startGame(){
 
   clearBoard();
 
-  gameRunning = true;
-
   updatePlayerNames();
 
+  currentPlayer = "X";
+
+  updateTurnColor();
+
   updateStatus();
+
+  gameRunning = true;
 }
 
 function handleCellClick(event){
 
-  const cell = event.target;
+  const cell =
+  event.target;
 
-  const index = cell.dataset.index;
+  const index =
+  cell.dataset.index;
 
-  if(board[index] !== "" || !gameRunning){
+  if(
+    board[index] !== ""
+    ||
+    !gameRunning
+  ){
     return;
   }
 
-  board[index] = currentPlayer;
+  board[index] =
+  currentPlayer;
 
-  cell.textContent = currentPlayer;
+  cell.textContent =
+  currentPlayer;
 
-  cell.classList.add(currentPlayer.toLowerCase());
+  cell.classList.add(
+    currentPlayer.toLowerCase()
+  );
 
   checkWinner();
 }
@@ -91,15 +149,33 @@ function checkWinner(){
 
   let roundWon = false;
 
-  for(let i = 0; i < winningCombinations.length; i++){
+  let winningCombo = [];
 
-    const combination = winningCombinations[i];
+  for(
+    let i = 0;
+    i < winningCombinations.length;
+    i++
+  ){
 
-    const a = board[combination[0]];
-    const b = board[combination[1]];
-    const c = board[combination[2]];
+    const combination =
+    winningCombinations[i];
 
-    if(a === "" || b === "" || c === ""){
+    const a =
+    board[combination[0]];
+
+    const b =
+    board[combination[1]];
+
+    const c =
+    board[combination[2]];
+
+    if(
+      a === ""
+      ||
+      b === ""
+      ||
+      c === ""
+    ){
       continue;
     }
 
@@ -107,9 +183,7 @@ function checkWinner(){
 
       roundWon = true;
 
-      combination.forEach(index => {
-        cells[index].classList.add("winner");
-      });
+      winningCombo = combination;
 
       break;
     }
@@ -117,27 +191,50 @@ function checkWinner(){
 
   if(roundWon){
 
-    const winnerName =
-      currentPlayer === "X"
-      ? nameX.textContent
-      : nameO.textContent;
+    winningCombo.forEach(index => {
 
-    statusText.innerHTML = `
-      ${winnerName} venceu!
-    `;
+      cells[index]
+      .classList
+      .add("winner");
+
+    });
+
+    const winnerName =
+
+      currentPlayer === "X"
+
+      ?
+
+      nameX.textContent
+
+      :
+
+      nameO.textContent;
+
+    winnerTitle.innerHTML =
+    `${winnerName} venceu!`;
+
+    winnerPopup.classList.add(
+      "show"
+    );
 
     if(currentPlayer === "X"){
 
       scoreX++;
 
-      scoreXText.textContent = scoreX;
+      scoreXText.textContent =
+      scoreX;
 
     }else{
 
       scoreO++;
 
-      scoreOText.textContent = scoreO;
+      scoreOText.textContent =
+      scoreO;
     }
+
+    statusText.innerHTML =
+    `${winnerName} venceu!`;
 
     gameRunning = false;
 
@@ -146,9 +243,15 @@ function checkWinner(){
 
   if(!board.includes("")){
 
-    statusText.innerHTML = `
-      EMPATE!
-    `;
+    winnerTitle.innerHTML =
+    "EMPATE!";
+
+    winnerPopup.classList.add(
+      "show"
+    );
+
+    statusText.innerHTML =
+    "EMPATE!";
 
     gameRunning = false;
 
@@ -161,41 +264,57 @@ function checkWinner(){
 function changePlayer(){
 
   currentPlayer =
-  currentPlayer === "X"
-  ? "O"
-  : "X";
 
-  updateStatus();
+  currentPlayer === "X"
+
+  ?
+
+  "O"
+
+  :
+
+  "X";
 
   updateTurnColor();
+
+  updateStatus();
 }
 
 function updateStatus(){
 
   const currentName =
+
     currentPlayer === "X"
-    ? nameX.textContent
-    : nameO.textContent;
 
-  statusText.innerHTML = `
-    Vez de ${currentName}
-  `;
+    ?
 
-  turnText.textContent = currentPlayer;
+    nameX.textContent
+
+    :
+
+    nameO.textContent;
+
+  statusText.innerHTML =
+  `Vez de ${currentName}`;
+
+  turnText.textContent =
+  currentPlayer;
 }
 
 function updateTurnColor(){
 
   if(currentPlayer === "X"){
 
-    turnText.style.color = "#38bdf8";
+    turnText.style.color =
+    "#38bdf8";
 
     turnText.style.textShadow =
     "0 0 15px #38bdf8";
 
   }else{
 
-    turnText.style.color = "#d16cff";
+    turnText.style.color =
+    "#d16cff";
 
     turnText.style.textShadow =
     "0 0 15px #d16cff";
@@ -205,14 +324,30 @@ function updateTurnColor(){
 function updatePlayerNames(){
 
   const playerXName =
-    playerXInput.value.trim() || "Jogador X";
+
+    playerXInput
+    .value
+    .trim()
+
+    ||
+
+    "Jogador X";
 
   const playerOName =
-    playerOInput.value.trim() || "Jogador O";
 
-  nameX.textContent = playerXName;
+    playerOInput
+    .value
+    .trim()
 
-  nameO.textContent = playerOName;
+    ||
+
+    "Jogador O";
+
+  nameX.textContent =
+  playerXName;
+
+  nameO.textContent =
+  playerOName;
 }
 
 function clearBoard(){
@@ -232,21 +367,43 @@ function clearBoard(){
     cell.classList.remove("winner");
 
   });
-
-  currentPlayer = "X";
-
-  updateTurnColor();
-
-  turnText.textContent = currentPlayer;
 }
 
 function restartGame(){
 
   clearBoard();
 
-  gameRunning = false;
+  winnerPopup.classList.remove(
+    "show"
+  );
 
-  statusText.innerHTML = `
-    Clique em iniciar
-  `;
+  currentPlayer = "X";
+
+  updateTurnColor();
+
+  updateStatus();
+
+  gameRunning = true;
 }
+
+function resetScore(){
+
+  scoreX = 0;
+
+  scoreO = 0;
+
+  scoreXText.textContent = 0;
+
+  scoreOText.textContent = 0;
+}
+
+function closeWinnerPopup(){
+
+  winnerPopup.classList.remove(
+    "show"
+  );
+
+  restartGame();
+}
+
+updateTurnColor();
